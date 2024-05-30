375d891273ee09d89378b8b530ec030ba3c01b25
Sunny <sunnyhong91@gmail.com>
2024-05-30 18:14:23 +0800

    Load image from disk instead of caching to avoid OOM

--- nerfstudio/data/datamanagers/full_images_datamanager.py
+++ nerfstudio/data/datamanagers/full_images_datamanager.py
@@ -129,87 +129,6 @@ class FullImageDatamanager(DataManager, Generic[TDataset]):
 
         super().__init__()
 
-    @cached_property
-    def cached_train(self) -> List[Dict[str, torch.Tensor]]:
-        """Get the training images. Will load and undistort the images the
-        first time this (cached) property is accessed."""
-        return self._load_images("train", cache_images_device=self.config.cache_images)
-
-    @cached_property
-    def cached_eval(self) -> List[Dict[str, torch.Tensor]]:
-        """Get the eval images. Will load and undistort the images the
-        first time this (cached) property is accessed."""
-        return self._load_images("eval", cache_images_device=self.config.cache_images)
-
-    def _load_images(
-        self, split: Literal["train", "eval"], cache_images_device: Literal["cpu", "gpu"]
-    ) -> List[Dict[str, torch.Tensor]]:
-        undistorted_images: List[Dict[str, torch.Tensor]] = []
-
-        # Which dataset?
-        if split == "train":
-            dataset = self.train_dataset
-        elif split == "eval":
-            dataset = self.eval_dataset
-        else:
-            assert_never(split)
-
-        def undistort_idx(idx: int) -> Dict[str, torch.Tensor]:
-            data = dataset.get_data(idx, image_type=self.config.cache_images_type)
-            camera = dataset.cameras[idx].reshape(())
-            assert data["image"].shape[1] == camera.width.item() and data["image"].shape[0] == camera.height.item(), (
-                f'The size of image ({data["image"].shape[1]}, {data["image"].shape[0]}) loaded '
-                f'does not match the camera parameters ({camera.width.item(), camera.height.item()})'
-            )
-            if camera.distortion_params is None or torch.all(camera.distortion_params == 0):
-                return data
-            K = camera.get_intrinsics_matrices().numpy()
-            distortion_params = camera.distortion_params.numpy()
-            image = data["image"].numpy()
-
-            K, image, mask = _undistort_image(camera, distortion_params, data, image, K)
-            data["image"] = torch.from_numpy(image)
-            if mask is not None:
-                data["mask"] = mask
-
-            dataset.cameras.fx[idx] = float(K[0, 0])
-            dataset.cameras.fy[idx] = float(K[1, 1])
-            dataset.cameras.cx[idx] = float(K[0, 2])
-            dataset.cameras.cy[idx] = float(K[1, 2])
-            dataset.cameras.width[idx] = image.shape[1]
-            dataset.cameras.height[idx] = image.shape[0]
-            return data
-
-        CONSOLE.log(f"Caching / undistorting {split} images")
-        with ThreadPoolExecutor(max_workers=2) as executor:
-            undistorted_images = list(
-                track(
-                    executor.map(
-                        undistort_idx,
-                        range(len(dataset)),
-                    ),
-                    description=f"Caching / undistorting {split} images",
-                    transient=True,
-                    total=len(dataset),
-                )
-            )
-
-        # Move to device.
-        if cache_images_device == "gpu":
-            for cache in undistorted_images:
-                cache["image"] = cache["image"].to(self.device)
-                if "mask" in cache:
-                    cache["mask"] = cache["mask"].to(self.device)
-        elif cache_images_device == "cpu":
-            for cache in undistorted_images:
-                cache["image"] = cache["image"].pin_memory()
-                if "mask" in cache:
-                    cache["mask"] = cache["mask"].pin_memory()
-        else:
-            assert_never(cache_images_device)
-
-        return undistorted_images
-
     def create_train_dataset(self) -> TDataset:
         """Sets up the data loaders for training"""
         return self.dataset_type(
@@ -264,7 +183,9 @@ class FullImageDatamanager(DataManager, Generic[TDataset]):
         Pretends to be the dataloader for evaluation, it returns a list of (camera, data) tuples
         """
         image_indices = [i for i in range(len(self.eval_dataset))]
-        data = deepcopy(self.cached_eval)
+        data = []
+        for i in image_indices:
+            data.append(self.eval_dataset.get_data(i, image_type=self.config.cache_images_type))
         _cameras = deepcopy(self.eval_dataset.cameras).to(self.device)
         cameras = []
         for i in image_indices:
@@ -293,7 +214,7 @@ class FullImageDatamanager(DataManager, Generic[TDataset]):
         if len(self.train_unseen_cameras) == 0:
             self.train_unseen_cameras = [i for i in range(len(self.train_dataset))]
 
-        data = deepcopy(self.cached_train[image_idx])
+        data = self.train_dataset.get_data(image_idx, image_type=self.config.cache_images_type)
         data["image"] = data["image"].to(self.device)
 
         assert len(self.train_dataset.cameras.shape) == 1, "Assumes single batch dimension"
@@ -319,7 +240,7 @@ class FullImageDatamanager(DataManager, Generic[TDataset]):
         # Make sure to re-populate the unseen cameras list if we have exhausted it
         if len(self.eval_unseen_cameras) == 0:
             self.eval_unseen_cameras = [i for i in range(len(self.eval_dataset))]
-        data = deepcopy(self.cached_eval[image_idx])
+        data = self.eval_dataset.get_data(image_idx, image_type=self.config.cache_images_type)
         data["image"] = data["image"].to(self.device)
         assert len(self.eval_dataset.cameras.shape) == 1, "Assumes single batch dimension"
         camera = self.eval_dataset.cameras[image_idx : image_idx + 1].to(self.device)
